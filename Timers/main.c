d8169f7ce3980955fae310b684812bf3ee3e27b5
Pedro Lucas <pedroca242@gmail.com>
2024-05-21 21:28:50 -0300

    Exemplo de debounce em software utilizando o timer 1 no modo ctc

--- Timers/main.c
+++ Timers/main.c
@@ -4,41 +4,52 @@
 
 #define F_CPU 16000000
 
-void setup_timer1(void){
-    TCNT1 = 65535 - 3125;
-
-    TCCR1A = 0x00;
-
-    // 16M/1024 = 15625 Hz
-    TCCR1B = (1 << CS12) | (1 << CS10);
-    
-}
-
 ISR(TIMER1_OVF_vect){
+    /*
+    * Desabilita a interrupção de overflow do timer 1
+    * Habilita a interrupção do INT0
+    */
     TIMSK1 &= ~(1 << TOIE1);
-    EIFR |= (1 << INTF0);
     EIMSK |= (1 << INT0);
 }
 
 ISR(INT0_vect){
+    /*
+    * Desabilita a interrupção do INT0
+    * Toggle no PB5
+    * Reseta o valor da contagem
+    * Habilita a interrrupção de overflow do timer 1
+    */
     EIMSK &= ~(1 << INT0);
     PINB = (1 << PB5);
-    TIMSK1 = (1 << TOIE1);
-    TCNT1 = 65535 - 3125;
+    TCNT1 = 0;
+    TIMSK1 |= (1 << TOIE1);
 }
 
 
 int main(void) {
+    /* Define o LED como saída */
     DDRB |= (1 << PB5);
 
+    /* Define INT0 como pull-up */
     DDRD &= ~(1 << PD2);
     PORTD |= (1 << PD2);
 
+    /* Define a interrupção como rising-edge
+     * Limpa a flag da interrupção
+     * Habilita a interrupção para o INT0 */
     EICRA |= (1 << ISC01) | (1 << ISC00);
     EIFR |= (1 << INTF0);
     EIMSK |= (1 << INT0);
 
-    setup_timer1();
+    /* 
+    * Define o valor de comparação
+    * Ativa o modo CTC
+    * Divide o clock
+    */
+    OCR1A = (F_CPU/1024)*0.2;
+    TCCR1A = (1 << WGM10);
+    TCCR1B = (1 << WGM12) | (1 << CS12) | (1 << CS10);
 
     sei();
 
