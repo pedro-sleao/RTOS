3b124711e92d92ad745725ea24c6ba0a146a4261
Pedro Lucas <pedroca242@gmail.com>
2024-06-29 16:12:16 -0300

    Começando a fazer a lista 1

--- Lista-1/main.c
+++ Lista-1/main.c
@@ -7,12 +7,29 @@
 
 #include <stdint.h>
 
+#define BUFFER_SIZE 255
+#define FOSC 16000000ul
+#define BAUD 57600
+#define MYUBRR (FOSC/(8ul*BAUD) - 1)
+
+/* Protótipos das funções */
 uint8_t write(uint8_t* buf, uint8_t n, int8_t close_packet);
 uint8_t read(uint8_t* buf, uint8_t n);
-void flow_off();
-void flow_on();
+uint8_t USART_Receive(void);
+void USART_Transmit(uint8_t data);
+void flow_off(void);
+void flow_on(void);
+void delay_ms(uint16_t ms);
+
+
+/* Variáveis globais */
+uint8_t rx_byte_cnt, tx_byte_cnt;
+uint8_t sinc = 0x7e, XON = 0x11, XOFF = 0x13, escape = 0x7d;
+uint8_t rx_state, rx_sinc, tx_state=1;
+uint8_t rx_data;
 
 ISR(USART_RX_vect) {
+   
 }
 
 ISR(USART_UDRE_vect) {
@@ -21,18 +38,24 @@ ISR(USART_UDRE_vect) {
 int main(void) {
     /* Faça a configuração do que for necessário aqui */
     /* Seu código aqui */
-    UCSR0A = 0;
-    UCSR0B = 0;
-    UCSR0C = 0;
-    UBRR0 = 1;
+    UCSR0A = (1 << U2X0); /* double speed */
+    UCSR0B = (1<<RXEN0) | (1 << TXEN0) | (1 << RXCIE0); /* Enable rx and tx, rx complete interrupt, and data register empty interrupt */
+    UCSR0C = (1<<UCSZ01) | (1<<UCSZ00); /* Set frame format: 8N1 */
+    UBRR0H = (MYUBRR>>8); /* Set baud rate */
+    UBRR0L = MYUBRR; /* Set baud rate */
+
+    DDRB |= (1 << PB5);
 
     sei();
 
     /* Implemente os testes aqui. Veja o texto para os detalhes */
+    uint8_t str_test2[] = "Universidade Federal de Pernambuco\nDepartamento de Eletrônica e Sistemas";
 
     /* Loop infinito necessário em qualquer programa para
        embarcados */
     while (1) {
+        write(str_test2, sizeof(str_test2), 1);
+        delay_ms(1000);
     }
 
     return 0;
@@ -40,15 +63,56 @@ int main(void) {
 
 
 uint8_t write(uint8_t* buf, uint8_t n, int8_t close_packet){
-    return 0;
+    tx_byte_cnt = 0;
+
+    if (!tx_state) {
+        return 0;
+    }
+    
+    while ((tx_byte_cnt < n) && tx_state) {
+        USART_Transmit(buf[tx_byte_cnt]);
+        tx_byte_cnt++;
+    }
+
+    if (close_packet) {
+        USART_Transmit(sinc);
+    }
+
+    return tx_byte_cnt;
 }
 
-uint8_t read(uint8_t* buf, uint8_t n){
-    return 0;
+uint8_t read(uint8_t* buf, uint8_t n) {
 }
 
-void flow_off(){
+uint8_t USART_Receive(void)
+{
+    /* Wait for data to be received */
+    while (!(UCSR0A & (1<<RXC0)))
+    ;
+    /* Get and return received data from buffer */
+    return UDR0;
 }
 
-void flow_on(){
+void USART_Transmit(uint8_t data)
+{
+    /* Wait for empty transmit buffer */
+    while (!(UCSR0A & (1<<UDRE0)));
+    /* Put data into buffer, sends the data */
+    UDR0 = data;
+}
+
+void flow_off(void) {
+    USART_Transmit(XOFF);
+}
+
+void flow_on(void) {
+    USART_Transmit(XON);
+}
+
+void delay_ms(uint16_t ms)
+{
+    uint16_t i, j;
+    for(i=0; i<ms; i++)
+        for(j=0; j<4000; j++)
+            _NOP();
 }
