244ebcc5481fd854d70a21cb5bcf61becd50bfea
Pedro Lucas <pedroca242@gmail.com>
2024-07-03 23:06:50 -0300

    Testes 3 e 4 da lista 1 (Falta corrigir diversas coisas)

--- Lista-1/main.c
+++ Lista-1/main.c
@@ -10,6 +10,7 @@
 #define FOSC 16000000ul
 #define BAUD 57600
 #define MYUBRR (FOSC/(8ul*BAUD) - 1)
+#define BUFFER_SIZE 255
 
 /* Caracteres especiais */
 #define SINC 0x7e
@@ -20,27 +21,72 @@
 /* Protótipos das funções */
 uint8_t write(uint8_t* buf, uint8_t n, int8_t close_packet);
 uint8_t read(uint8_t* buf, uint8_t n);
-uint8_t USART_Receive(void);
-void USART_Transmit(uint8_t data);
+uint16_t read_rx_buffer(void);
+uint8_t write_tx_buffer(uint8_t c);
 void flow_off(void);
 void flow_on(void);
+uint8_t is_flow_on(void);
 void delay_ms(uint16_t ms);
+void blink_led(void);
+void turn_led_on(void);
 
 
 /* Variáveis globais */
 uint8_t rx_byte_cnt, tx_byte_cnt;
-uint8_t rx_state, rx_sinc, tx_state=1;
+uint8_t rx_esc, rx_state=1, rx_sinc, tx_state=1;
+uint8_t rx_buffer[BUFFER_SIZE]; /* buffer para recepção */
+uint8_t rx_head, rx_tail; /* ponteiros para o buffer circular */
+uint8_t tx_buffer[BUFFER_SIZE];     /* buffer para transmissão */
+uint8_t tx_head, tx_tail;   /* ponteiros para o buffer circular */
+uint8_t usart_transmitting;
 
+volatile uint8_t rcvd_byte=0;
 ISR(USART_RX_vect) {
-   uint8_t received_byte = UDR0;
-    if (received_byte == XOFF) {
+    uint8_t received_data = UDR0;
+    
+    if (received_data == ESC && !rx_esc) {
+        rx_esc = 1;
+    } else if (received_data == XOFF && !rx_esc) {
         tx_state = 0;
-    } else if (received_byte == XON) {
+    } else if (received_data == XON && !rx_esc) {
         tx_state = 1;
+    } else if (received_data == SINC && !rx_esc){
+        rx_sinc = 1;
+    } else {
+        rx_esc = 0;
+
+        uint8_t next = rx_head + 1;
+        if (next >= BUFFER_SIZE)
+            next = 0;
+        
+        /* Se o buffer estiver cheio desprezamos o byte recebido mais antigo e
+        * colocamos o byte que acabamos de receber em seu lugar */
+        rx_buffer[rx_head] = received_data;
+        rx_head = next;
+        if (next == rx_tail) { /* buffer cheio */
+            rx_tail++;
+            if (rx_tail >= BUFFER_SIZE)
+                rx_tail = 0;
+        }
+        rcvd_byte = 1;
     }
 }
 
-ISR(USART_UDRE_vect) {
+/* Flag UDREn indica se o buffer de transmissão UDRn está vazio */
+ISR(USART_UDRE_vect)
+{
+    /*
+     * Caso tx_head == tx_tail é porque todos os dados foram transmitidos e desabilita a interrupção para UDREn
+     * Caso contrário envia o dado que estava no tx_tail
+     */
+    if (tx_head == tx_tail) {
+        UCSR0B &= ~(1 << UDRIE0);
+        usart_transmitting = 0;
+    } else {
+        UDR0 = tx_buffer[tx_tail];
+        if (++tx_tail >= BUFFER_SIZE)
+            tx_tail = 0;
+    }
 }
 
 int main(void) {
@@ -58,14 +104,42 @@ int main(void) {
     sei();
 
     /* Implemente os testes aqui. Veja o texto para os detalhes */
+
+    /* Teste 2 */
     uint8_t str_test[] = "Universidade Federal de Pernambuco\nDepartamento de Eletrônica e Sistemas";
+    write(str_test, sizeof(str_test)-1, 1);
+
+    /* Teste 3*/
+    uint8_t i = sizeof(str_test);
+    uint8_t j = 0;
+    while (i) {
+        if (i >= 10) {
+            i -= write(str_test+j, 10, 0);
+            j += 10;
+        } else {
+            write(str_test+j, i, 1);
+            i = 0;
+        }
+    }
+
+    /* Teste 4 */
+    uint16_t nbytes = 0;
+    uint8_t buffer[255];
+    while (nbytes < 300) {
+        if (rcvd_byte) {
+            nbytes += read(buffer, 1);
+        }
+    }
+    flow_off();
+    if (is_flow_on()) {
+        turn_led_on();
+    } else {
+        blink_led();
+    }
 
     /* Loop infinito necessário em qualquer programa para
        embarcados */
-    while (1) {
-        write(str_test, sizeof(str_test)-1, 1);
-        delay_ms(1000);
-    }
+    while (1) {}
 
     return 0;
 }
@@ -79,43 +153,96 @@ uint8_t write(uint8_t* buf, uint8_t n, int8_t close_packet){
     }
     
     while ((tx_byte_cnt < n) && tx_state) {
-        USART_Transmit(buf[tx_byte_cnt]);
+        write_tx_buffer(buf[tx_byte_cnt]);
         tx_byte_cnt++;
     }
 
     if (close_packet && tx_state) {
-        USART_Transmit(SINC);
+        write_tx_buffer(SINC);
     }
 
     return tx_byte_cnt;
 }
 
 uint8_t read(uint8_t* buf, uint8_t n) {
+    uint8_t received_byte, byte_cnt = 0;
+    static uint8_t byte_pos = 0;
+
+    while (byte_cnt <= n) { 
+        if (!rx_state) {
+            return 0;
+        }
+        received_byte = read_rx_buffer();
+
+        if (!rx_esc) {
+            if (received_byte == SINC) {
+                buf[byte_pos] = '\0';
+                byte_pos = 0;
+                return byte_cnt;
+            } else if (received_byte != XON && received_byte != XOFF && received_byte != ESC) {
+                buf[byte_pos] = received_byte;
+                byte_cnt++;
+                byte_pos++;
+            }
+        } else {
+            buf[byte_pos] = received_byte;
+            byte_cnt++;
+            byte_pos++;
+        }
+    }
+    return n+1;
 }
 
-uint8_t USART_Receive(void)
+uint16_t read_rx_buffer(void)
 {
-    /* Wait for data to be received */
-    while (!(UCSR0A & (1<<RXC0)))
-    ;
-    /* Get and return received data from buffer */
-    return UDR0;
+    uint8_t rc = 0, c = 0;
+
+    cli();
+    if (rx_head != rx_tail) {
+        c = rx_buffer[rx_tail];
+        if (++rx_tail >= BUFFER_SIZE)
+            rx_tail = 0;
+    } else 
+        rc = 1;
+    sei();
+    return ((uint16_t) rc << 8) | c;
 }
 
-void USART_Transmit(uint8_t data)
-{
-    /* Wait for empty transmit buffer */
-    while (!(UCSR0A & (1<<UDRE0)));
-    /* Put data into buffer, sends the data */
-    UDR0 = data;
+uint8_t write_tx_buffer(uint8_t c) {
+    uint8_t rc = 0;
+
+    cli();
+
+    if(!usart_transmitting){
+        UDR0 = c;
+        UCSR0B |= (1 << UDRIE0);
+        usart_transmitting = 1;
+    } else{
+        uint8_t next = tx_head + 1;
+        if (next >= BUFFER_SIZE)
+            next = 0;
+        if (next != tx_tail) {
+            tx_buffer[tx_head] = c;
+            tx_head = next;
+        } else  /* buffer cheio */
+            rc = 1;
+    }
+    sei();
+    return rc;
 }
 
 void flow_off(void) {
-    USART_Transmit(XOFF);
+    write_tx_buffer(XOFF);
+    rx_state = 0;
 }
 
 void flow_on(void) {
-    USART_Transmit(XON);
+    write_tx_buffer(XON);
+    rx_state = 1;
+}
+
+uint8_t is_flow_on(void) {
+    return tx_state;
 }
 
 void delay_ms(uint16_t ms)
@@ -124,4 +251,18 @@ void delay_ms(uint16_t ms)
     for(i=0; i<ms; i++)
         for(j=0; j<4000; j++)
             _NOP();
+}
+
+void blink_led(void) {
+    uint8_t k = 5;
+    while (k--) {
+        PINB = (1 << PB5);
+        delay_ms(1000);
+    }
+}
+
+void turn_led_on(void) {
+    PORTB |= (1 << PB5);
+    delay_ms(5000);
+    PORTB &= ~(1 << PB5);
 }
