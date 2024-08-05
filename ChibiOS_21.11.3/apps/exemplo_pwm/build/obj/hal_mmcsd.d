9f9e1b91eb7ee116e35dd32a01401d6e380ffc78
Pedro Lucas <pedroca242@gmail.com>
2024-08-05 16:59:55 -0300

    Começando a utilizar o chibios

---
+++ ChibiOS_21.11.3/apps/exemplo_pwm/build/obj/hal_mmcsd.d
@@ -0,0 +1,244 @@
+build/obj/hal_mmcsd.o: ../../os/hal/src/hal_mmcsd.c \
+ ../../os/hal/include/hal.h ../../os/common/portability/GCC/ccportab.h \
+ ../../os/hal/osal/rt-nil/osal.h ../../os/rt/include/ch.h \
+ ../../os/license/chlicense.h ../../os/license/chversion.h \
+ ../../os/license/chcustomer.h cfg/chconf.h \
+ ../../os/rt/include/chchecks.h ../../os/rt/include/chrestrictions.h \
+ ../../os/rt/include/chearly.h \
+ ../../os/common/ports/AVR/compilers/GCC/chtypes.h \
+ ../../os/rt/include/chrfcu.h ../../os/rt/include/chdebug.h \
+ ../../os/rt/include/chtime.h ../../os/rt/include/chlists.h \
+ ../../os/rt/include/chalign.h ../../os/rt/include/chtrace.h \
+ ../../os/rt/include/chport.h ../../os/common/ports/AVR/chcore.h \
+ ../../os/hal/osal/rt-nil/chcore_timer.h ../../os/rt/include/chtm.h \
+ ../../os/rt/include/chstats.h ../../os/rt/include/chobjects.h \
+ ../../os/rt/include/chsys.h ../../os/rt/include/chinstances.h \
+ ../../os/rt/include/chvt.h ../../os/rt/include/chschd.h \
+ ../../os/rt/include/chthreads.h ../../os/rt/include/chregistry.h \
+ ../../os/rt/include/chsem.h ../../os/rt/include/chmtx.h \
+ ../../os/rt/include/chcond.h ../../os/rt/include/chevents.h \
+ ../../os/rt/include/chmsg.h ../../os/oslib/include/chlib.h \
+ ../../os/oslib/include/chbsem.h ../../os/oslib/include/chmboxes.h \
+ ../../os/oslib/include/chmemcore.h ../../os/oslib/include/chmemheaps.h \
+ ../../os/oslib/include/chmempools.h ../../os/oslib/include/chobjfifos.h \
+ ../../os/oslib/include/chpipes.h ../../os/oslib/include/chobjcaches.h \
+ ../../os/oslib/include/chdelegates.h ../../os/oslib/include/chjobs.h \
+ ../../os/oslib/include/chfactory.h ../../os/rt/include/chdynamic.h \
+ ../../os/hal/boards/ARDUINO_NANO/board.h cfg/halconf.h cfg/mcuconf.h \
+ ../../os/hal/ports/AVR/MEGA/ATMEGAxx/hal_lld.h \
+ ../../os/hal/include/hal_objects.h ../../os/hal/include/hal_streams.h \
+ ../../os/hal/include/hal_channels.h ../../os/hal/include/hal_files.h \
+ ../../os/hal/include/hal_ioblock.h ../../os/hal/include/hal_mmcsd.h \
+ ../../os/hal/include/hal_persistent.h ../../os/hal/include/hal_flash.h \
+ ../../os/hal/include/hal_buffers.h ../../os/hal/include/hal_queues.h \
+ ../../os/hal/include/hal_buffered_serial.h \
+ ../../os/hal/include/hal_pal.h \
+ ../../os/hal/ports/AVR/MEGA/LLD/GPIOv1/hal_pal_lld.h \
+ ../../os/hal/ports/AVR/MEGA/ATMEGAxx/avr_pins.h \
+ ../../os/hal/include/hal_adc.h ../../os/hal/include/hal_can.h \
+ ../../os/hal/include/hal_crypto.h ../../os/hal/include/hal_dac.h \
+ ../../os/hal/include/hal_efl.h ../../os/hal/include/hal_gpt.h \
+ ../../os/hal/include/hal_i2c.h ../../os/hal/include/hal_i2s.h \
+ ../../os/hal/include/hal_icu.h ../../os/hal/include/hal_mac.h \
+ ../../os/hal/include/hal_pwm.h \
+ ../../os/hal/ports/AVR/MEGA/LLD/TIMv1/hal_pwm_lld.h \
+ ../../os/hal/ports/AVR/MEGA/ATMEGAxx/avr_timers.h cfg/mcuconf.h \
+ ../../os/hal/include/hal_rtc.h ../../os/hal/include/hal_serial.h \
+ ../../os/hal/ports/AVR/MEGA/LLD/USARTv1/hal_serial_lld.h \
+ ../../os/hal/include/hal_sdc.h ../../os/hal/include/hal_sio.h \
+ ../../os/hal/include/hal_spi.h ../../os/hal/include/hal_trng.h \
+ ../../os/hal/include/hal_uart.h ../../os/hal/include/hal_usb.h \
+ ../../os/hal/include/hal_wdg.h ../../os/hal/include/hal_wspi.h \
+ ../../os/hal/include/hal_st.h \
+ ../../os/hal/ports/AVR/MEGA/LLD/SYSTICKv1/hal_st_lld.h \
+ ../../os/hal/include/hal_mmc_spi.h ../../os/hal/include/hal_serial_usb.h
+
+../../os/hal/include/hal.h:
+
+../../os/common/portability/GCC/ccportab.h:
+
+../../os/hal/osal/rt-nil/osal.h:
+
+../../os/rt/include/ch.h:
+
+../../os/license/chlicense.h:
+
+../../os/license/chversion.h:
+
+../../os/license/chcustomer.h:
+
+cfg/chconf.h:
+
+../../os/rt/include/chchecks.h:
+
+../../os/rt/include/chrestrictions.h:
+
+../../os/rt/include/chearly.h:
+
+../../os/common/ports/AVR/compilers/GCC/chtypes.h:
+
+../../os/rt/include/chrfcu.h:
+
+../../os/rt/include/chdebug.h:
+
+../../os/rt/include/chtime.h:
+
+../../os/rt/include/chlists.h:
+
+../../os/rt/include/chalign.h:
+
+../../os/rt/include/chtrace.h:
+
+../../os/rt/include/chport.h:
+
+../../os/common/ports/AVR/chcore.h:
+
+../../os/hal/osal/rt-nil/chcore_timer.h:
+
+../../os/rt/include/chtm.h:
+
+../../os/rt/include/chstats.h:
+
+../../os/rt/include/chobjects.h:
+
+../../os/rt/include/chsys.h:
+
+../../os/rt/include/chinstances.h:
+
+../../os/rt/include/chvt.h:
+
+../../os/rt/include/chschd.h:
+
+../../os/rt/include/chthreads.h:
+
+../../os/rt/include/chregistry.h:
+
+../../os/rt/include/chsem.h:
+
+../../os/rt/include/chmtx.h:
+
+../../os/rt/include/chcond.h:
+
+../../os/rt/include/chevents.h:
+
+../../os/rt/include/chmsg.h:
+
+../../os/oslib/include/chlib.h:
+
+../../os/oslib/include/chbsem.h:
+
+../../os/oslib/include/chmboxes.h:
+
+../../os/oslib/include/chmemcore.h:
+
+../../os/oslib/include/chmemheaps.h:
+
+../../os/oslib/include/chmempools.h:
+
+../../os/oslib/include/chobjfifos.h:
+
+../../os/oslib/include/chpipes.h:
+
+../../os/oslib/include/chobjcaches.h:
+
+../../os/oslib/include/chdelegates.h:
+
+../../os/oslib/include/chjobs.h:
+
+../../os/oslib/include/chfactory.h:
+
+../../os/rt/include/chdynamic.h:
+
+../../os/hal/boards/ARDUINO_NANO/board.h:
+
+cfg/halconf.h:
+
+cfg/mcuconf.h:
+
+../../os/hal/ports/AVR/MEGA/ATMEGAxx/hal_lld.h:
+
+../../os/hal/include/hal_objects.h:
+
+../../os/hal/include/hal_streams.h:
+
+../../os/hal/include/hal_channels.h:
+
+../../os/hal/include/hal_files.h:
+
+../../os/hal/include/hal_ioblock.h:
+
+../../os/hal/include/hal_mmcsd.h:
+
+../../os/hal/include/hal_persistent.h:
+
+../../os/hal/include/hal_flash.h:
+
+../../os/hal/include/hal_buffers.h:
+
+../../os/hal/include/hal_queues.h:
+
+../../os/hal/include/hal_buffered_serial.h:
+
+../../os/hal/include/hal_pal.h:
+
+../../os/hal/ports/AVR/MEGA/LLD/GPIOv1/hal_pal_lld.h:
+
+../../os/hal/ports/AVR/MEGA/ATMEGAxx/avr_pins.h:
+
+../../os/hal/include/hal_adc.h:
+
+../../os/hal/include/hal_can.h:
+
+../../os/hal/include/hal_crypto.h:
+
+../../os/hal/include/hal_dac.h:
+
+../../os/hal/include/hal_efl.h:
+
+../../os/hal/include/hal_gpt.h:
+
+../../os/hal/include/hal_i2c.h:
+
+../../os/hal/include/hal_i2s.h:
+
+../../os/hal/include/hal_icu.h:
+
+../../os/hal/include/hal_mac.h:
+
+../../os/hal/include/hal_pwm.h:
+
+../../os/hal/ports/AVR/MEGA/LLD/TIMv1/hal_pwm_lld.h:
+
+../../os/hal/ports/AVR/MEGA/ATMEGAxx/avr_timers.h:
+
+cfg/mcuconf.h:
+
+../../os/hal/include/hal_rtc.h:
+
+../../os/hal/include/hal_serial.h:
+
+../../os/hal/ports/AVR/MEGA/LLD/USARTv1/hal_serial_lld.h:
+
+../../os/hal/include/hal_sdc.h:
+
+../../os/hal/include/hal_sio.h:
+
+../../os/hal/include/hal_spi.h:
+
+../../os/hal/include/hal_trng.h:
+
+../../os/hal/include/hal_uart.h:
+
+../../os/hal/include/hal_usb.h:
+
+../../os/hal/include/hal_wdg.h:
+
+../../os/hal/include/hal_wspi.h:
+
+../../os/hal/include/hal_st.h:
+
+../../os/hal/ports/AVR/MEGA/LLD/SYSTICKv1/hal_st_lld.h:
+
+../../os/hal/include/hal_mmc_spi.h:
+
+../../os/hal/include/hal_serial_usb.h:
